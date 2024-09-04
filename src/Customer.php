fea29382c4468d7f2e3b9dd5e0b0ef15743c84c0
ikhsanurasidb <muhammad.nurasid@binus.ac.id>
2024-09-04 22:33:12 +0700

    Update Test

--- src/Customer.php
+++ src/Customer.php
@@ -4,7 +4,7 @@ namespace kuraninja\Belajar;
 
 class Customer
 {
-    public function __construct(private string $name)
+    public function __construct(private string $name = "Kamu")
     {
     }
 
